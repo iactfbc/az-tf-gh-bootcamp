3af28807a6313f2bba213f211e6c3387f020edc3
Roland Hernandez <rohernan@microsoft.com>
2022-12-02 15:40:44 -0500

    resolved incorrect syntax

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,7 +86,7 @@ resource "azurerm_subnet" "bcserversubnet" {
 
 
 resource "azurerm_network_interface" "bcvmnic" {
-  name                = "${bc_vm_name}-nic"
+  name                = "${var.bc_vm_name}-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
